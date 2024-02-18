e1e2c2fa3a9694fc04d6a6509640f3c7f0982b61
Chris Johnson <chris@rebzmedia.com>
2024-02-18 17:24:56 -0500

    add dashboard content

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <h1>Dashboard</h1>
+    <p>Welcome to your dashboard</p>
+
+    <div class="grid">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
 
   </div>
 </template>
@@ -7,4 +18,17 @@
 <script setup>
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 16px;
+  margin-top: 32px;
+
+  div {
+    background: rgba(0,0,0,0.1);
+    height: 100px;
+    border-radius: 8px;
+  }
+}
+</style>
